ca453e48c5393ac036a9d416ab9a064a215e3322
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-27 19:48:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,6 +10,8 @@ class App < Sinatra::Base
 
 #post '/' do, which receives the form data through params and renders the results page.
   post '/' do
+    text_from_user = params[:user_text]
+
 #To use the model we've created with our controller, we need to connect the two. In order to do this, we'll add a require_relative statement to the controller so that this file can use this new class.. At the top of app.rb, add require_relative "models/text_analyzer.rb". This now gives us the ability to reference the TextAnalyzer class and invoke its new method.
 #data from params[:user_text] (in the post '/' do route) and feed it into a new instance of the TextAnalyzer class:
   @analyzed_text = TextAnalyzer.new(params[:user_text])
