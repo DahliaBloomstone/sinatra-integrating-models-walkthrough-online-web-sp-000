43f7e4bd109437d3306b722e1cda228f3a7b5a18
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-27 19:41:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,9 +2,13 @@ require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
 class App < Sinatra::Base
+
+#get '/' do, which renders the index.erb page.
   get '/' do
     erb :index
   end
+
+#post '/' do, which receives the form data through params and renders the results page.
 
   post '/' do
     text_from_user = params[:user_text]
