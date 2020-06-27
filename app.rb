349114c3cde6ad7973adc8c423a89714c89c8b2f
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-27 19:41:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,6 @@ class App < Sinatra::Base
 #post '/' do, which receives the form data through params and renders the results page.
   post '/' do
     text_from_user = params[:user_text]
-
     erb :results
   end
 end
